0f369910a4a4e41445b6b2130bd060e8c0cd9c0b
Douglass Flowers <dtflowers@gmail.com>
2016-07-12 09:37:42 -0500

    add completed assignment with extra credit

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function learnMore() {
+    alert("This is where the link to learn more goes.");
+}		
+function firstStory() {
+    alert("This is where the link to the CBS News story on Dr. Andrews goes.");
+}	
+function secondStory() {
+    alert("This is where the link to whatever Jersey Watch Network is goes.");
+}
+function thirdStory() {
+    alert("This is where the link to the content about Tommy John surgery, the countless Braves pitchers who have it every year, a cautionary tale, etc. goes.");
+}					
+                            
